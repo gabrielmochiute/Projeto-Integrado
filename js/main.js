f0b114fea643b1060c3de6319a36f44e8c87f4a4
INF1A-EAD <INF1A-EAD@cfp127aluno.local>
2021-08-12 21:56:24 -0300

    Animations

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 100);
   logo.classList.toggle("transparentLogo", window.scrollY < 100);
 });
+
+document.addEventListener("mousemove", parallax);
+
+function parallax(e) {
+  this.querySelectorAll(".layer").forEach((layer) => {
+    const speed = layer.getAttribute("data-speed");
+
+    const x = (window.innerWidth - e.pageX * speed) / 100;
+    const y = (window.innerHeight - e.pageY * speed) / 100;
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+}
